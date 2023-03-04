d52106542400ceb7b9995e1275c6c8080582b3ae
Ardian Maulana <exbeelh@gmail.com>
2023-03-04 10:47:50 +0700

    fix accessibility issue with adding semantic html

--- src/App.jsx
+++ src/App.jsx
@@ -13,16 +13,18 @@ function App() {
   }
 
   return (
-    <div className="flex h-[100vh]">
-      <div className="relative container lg:max-w-xs max-w-[12rem] lg:max-h-[52rem] max-h-[72rem] bg-[#313a49] text-center mx-auto my-auto rounded-xl drop-shadow-xl lg:p-5 p-3">
-        <p className="text-sm mb-3 text-[#51fea9] py-3 lg:py-1">A D V I C E  #{advice.id}</p>
-        <h1 className="text-cyan-100 text-base">"{advice.messages}"</h1>
-        <img className="mx-auto py-6" src={"pattern-divider-desktop.svg"} alt="Devider" />
-        <button className="absolute bottom-0 lg:left-[8.76rem] left-[5rem] h-10 w-10 rounded-full translate-y-[30px] bg-[#51fea9] hover:shadow-lg hover:shadow-[#51fea9]" onClick={handleClick}>
-          <img className="mx-auto" src={"icon-dice.svg"} alt="Dice" />
-        </button>
+    <main>
+      <div className="flex h-[100vh]">
+        <div className="relative container lg:max-w-xs max-w-[12rem] lg:max-h-[52rem] max-h-[72rem] bg-[#313a49] text-center mx-auto my-auto rounded-xl drop-shadow-xl lg:p-5 p-3">
+          <p className="text-sm mb-3 text-[#51fea9] py-3 lg:py-1">A D V I C E  #{advice.id}</p>
+          <h1 className="text-cyan-100 text-base">"{advice.messages}"</h1>
+          <img className="mx-auto py-6" src={"pattern-divider-desktop.svg"} alt="Devider" />
+          <button className="absolute bottom-0 lg:left-[8.76rem] left-[5rem] h-10 w-10 rounded-full translate-y-[30px] bg-[#51fea9] hover:shadow-lg hover:shadow-[#51fea9]" onClick={handleClick}>
+            <img className="mx-auto" src={"icon-dice.svg"} alt="Dice" />
+          </button>
+        </div>
       </div>
-    </div>
+    </main>
   );
 }
 
